c4fb6a5013e793f9ea4f6944689b2e6634c73777
n-niewiadowska <143649665+n-niewiadowska@users.noreply.github.com>
2024-01-31 21:22:44 +0000

    [[backend]]: sentry configuration

--- server/server.js
+++ server/server.js
@@ -4,9 +4,22 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const mqtt = require("mqtt");
+const Sentry = require("@sentry/node");
+const { ProfilingIntegration } = require("@sentry/profiling-node");
 
 const app = express();
 require("dotenv").config({ path: "./config.env" });
+
+Sentry.init({
+  dsn: "https://6708d332f5567a40b8cb8e0e6fc7beaf@o4506638833942528.ingest.sentry.io/4506638836891648",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
 
 const port = process.env.PORT || 5000;
 const mongoUri = process.env.MONGO_URI;
@@ -27,6 +40,9 @@ mongoose.connect(mongoUri)
     console.log("MongoDB connection established!");
   })
   .catch(err => console.log(err));
+  
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors({
   origin: "http://localhost:3000",
@@ -40,9 +56,21 @@ app.use("/users", require("./routes/user"));
 app.use("/services", require("./routes/service"));
 app.use("/appointments", require("./routes/appointment"));
 app.use("/opinions", require("./routes/opinion"));
+
+app.get("/", (req, res) => {
+  res.end("This is a Beauty Salon website!");
+});
+
+// verifying sentry
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
+
 app.use((req, res) => {
   res.redirect("https://http.cat/404");
 });
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`);
